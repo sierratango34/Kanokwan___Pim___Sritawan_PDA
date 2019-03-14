a0099fc64116a8c77e037f579004cb32425e8f55
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-03-14 09:33:44 +0000

    finished Week2

---
+++ PDA_evidence.rb
@@ -0,0 +1,46 @@
+#I.T.6
+
+@drinks = {
+  gin: "Edinburgh Gin",
+  vodka: "Smirnoff",
+  beer: "Brewdog",
+  whisky: "The Famous Grouse"
+}
+
+def find_gin
+  return @drinks[:gin]
+end
+
+p find_gin
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#I.T.5
+
+# @names_in_class = ["Kanokwan Sritawan", "Stephen Gibson", "Jennifer Archibald", "Mark King"]
+#
+# def add_name(name)
+#   @names_in_class.push(name)
+# end
+#
+# p @names_in_class
+# p add_name("Becky Nielson")
