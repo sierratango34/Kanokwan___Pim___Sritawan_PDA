5bf294d683f0e8fdde8492006710946f11d2a619
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-05-01 20:13:34 +0100

    update the display of the running total integration test working

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -1,19 +1,29 @@
-var chai = require('chai');
-var chaiAsPromised = require('chai-as-promised');
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
-var expect = chai.expect;
+const expect = chai.expect;
+const should = chai.should();
 
-describe('calculator functionality', function() {
+describe("calculator functionality", function() {
   beforeEach(function() {
     browser.ignoreSynchronization = true;
-    browser.get('http://localhost:3000');
+    browser.get("http://localhost:3000");
   });
 
   // write integration tests here in the form of "it should do something..."
-  it('should have working number buttons', function(){
-    running_total = element(by.css('#running_total'))
-    element(by.css('#number2')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('2')
-  })
+  it("should have working number buttons", function() {
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number2")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal("2");
+  });
 
+  it("update the display of the running total", function() {
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number2")).click();
+    element(by.css("#operator_add")).click();
+    element(by.css("#number8")).click();
+    element(by.css("#operator_equals")).click();
+
+    expect(running_total.getAttribute("value")).to.eventually.equal("10");
+  });
 });
