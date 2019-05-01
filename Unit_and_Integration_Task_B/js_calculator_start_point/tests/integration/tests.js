47d6b91c324f21715e02f2ea8c88e1951df87ec3
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-05-01 20:38:51 +0100

    calculator modified, it displays not a number when dividing by zero integration test working

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -86,4 +86,13 @@ describe("calculator functionality", function() {
       "99999999999999"
     );
   });
+
+  it("it displays not a number when dividing by zero", function() {
+    running_total = element(by.css("#running_total"));
+    element(by.css("#number2")).click();
+    element(by.css("#operator_divide")).click();
+    element(by.css("#number0")).click();
+    element(by.css("#operator_equals")).click();
+    expect(running_total.getAttribute("value")).to.eventually.equal("0");
+  });
 });
